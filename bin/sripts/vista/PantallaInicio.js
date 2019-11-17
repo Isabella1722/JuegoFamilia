a0229c7388a5ed52e12a1068f337138b18bab763
Isabella1722 <isabellalinarescamacho0517@gmail.com>
2019-11-17 18:50:54 -0500

    se realizaron cambios y el contador

--- bin/sripts/vista/PantallaInicio.js
+++ bin/sripts/vista/PantallaInicio.js
@@ -1,12 +1,29 @@
-class PantallaInicio{
-    constructor(){
 
-    
+
+
+let btnIngresar = document.querySelector("#botonIngresar");
+
+let cont = 5;
+
+/*
+let contadorHilo = setInterval(function(){
+
+
+    if(cont==0){
+        document.location.href="./bin/views/PantallaRegistro.html"
     }
-
-    
-
-
+    cont-=1;
+},1000);*/
 
 
-}
+
+
+btnIngresar.addEventListener("click", function(){
+
+    document.location.href="./bin/views/PantallaDesbloqueo1.html"
+
+ 
+
+
+})
+
---
+++ bin/sripts/vista/pantallaDesbloqueo1.js
@@ -0,0 +1,29 @@
+let contador = document.querySelector(".cuentaRegresiva")
+let segundos = 5;
+contador.textContent = segundos;
+segundos -= 1;
+
+
+
+
+
+let hiloPorSegundo = setInterval(function () {
+
+    contador.textContent = segundos;
+    if (segundos > 0) segundos -= 1;
+
+
+    if (segundos == 0) {
+
+        contador.classList.add("animated")
+        contador.classList.add("heartBeat")
+        contador.style.fontSize = "70px"
+        contador.style.backgroundColor = "#FFFFFF"
+        contador.style.color = "rgb(202, 118, 103)"
+
+        contador.textContent = "¡VAMOS!"
+    }
+
+}, 1000)
+
+
