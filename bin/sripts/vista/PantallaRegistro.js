4658f47de425adfae7b3b00c0dbbd78114cfe06e
stban2703 <s.t.ban1401@gmail.com>
2019-11-19 11:46:15 -0500

    Modal

--- bin/sripts/vista/PantallaRegistro.js
+++ bin/sripts/vista/PantallaRegistro.js
@@ -1,6 +1,9 @@
 
 
 let btnRegistrar = document.querySelector("#botonRegistrar");
+let palabra = document.querySelector("#textoCheck")
+let contenedorModal = document.querySelector(".modalContenedor")
+let modal = document.querySelector(".modal")
 
 let cont = 5;
 
@@ -13,16 +16,29 @@ let contadorHilo = setInterval(function(){
     }
     cont-=1;
 },1000);*/
+palabra.addEventListener("click", function () {
+    modal.classList.add("open")
+    contenedorModal.classList.add("open")
+
+})
+
+document.addEventListener("click", function (event) {
+   if(event.target==contenedorModal){
+    modal.classList.remove("open")
+    contenedorModal.classList.remove("open")
+   }
+
+
+})
 
 
 
+btnRegistrar.addEventListener("click", function () {
 
-btnRegistrar.addEventListener("click", function(){
+    document.location.href = "./../../bin/views/instruccion1.html"
 
-    document.location.href="./../../bin/views/instruccion1.html"
-    
 
- 
+
 
 
 })
