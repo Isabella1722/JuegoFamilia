6ddba7967200687650ce63df0e1da61aaa94cb42
Isabella1722 <isabellalinarescamacho0517@gmail.com>
2019-11-19 08:18:09 -0500

    se creó la pantalla registro

---
+++ bin/sripts/vista/PantallaRegistro.js
@@ -0,0 +1,27 @@
+
+
+let btnRegistrar = document.querySelector("#botonRegistrar");
+
+let cont = 5;
+
+/*
+let contadorHilo = setInterval(function(){
+
+
+    if(cont==0){
+        document.location.href="./bin/views/PantallaRegistro.html"
+    }
+    cont-=1;
+},1000);*/
+
+
+
+
+btnRegistrar.addEventListener("click", function(){
+
+    document.location.href="./bin/views/instruccion1.html"
+
+ 
+
+
+})
