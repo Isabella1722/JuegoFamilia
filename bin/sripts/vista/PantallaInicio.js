c086e84d22cd50f4249a005314bff88e06e53f09
stban2703 <s.t.ban1401@gmail.com>
2019-11-20 12:37:57 -0500

    Login y registro completo

--- bin/sripts/vista/PantallaInicio.js
+++ bin/sripts/vista/PantallaInicio.js
@@ -1,6 +1,20 @@
 
+var firebaseConfig = {
+    apiKey: "AIzaSyBohk7vWZEwnYrclr8UdsfZPDwArRigMqo",
+    authDomain: "familiapi.firebaseapp.com",
+    databaseURL: "https://familiapi.firebaseio.com",
+    projectId: "familiapi",
+    storageBucket: "familiapi.appspot.com",
+    messagingSenderId: "943521700619",
+    appId: "1:943521700619:web:a117acd032fe01862de00e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
+let correoInput = document.querySelector('#emailLogin')
+let passInput = document.querySelector('#passwordLogin')
 let btnIngresar = document.querySelector("#botonIngresar");
 let ping
 let cont = 5;
@@ -11,7 +25,7 @@ function preload() {
 }
 
 function setup() {
- /*ping.loop();*/
+    /*ping.loop();*/
 }
 
 /*
@@ -28,15 +42,46 @@ let contadorHilo = setInterval(function(){
 
 
 btnIngresar.addEventListener("click", function () {
-    ping.play();
-
-    let retrasador = setTimeout(function () {
-        document.location.href = "./bin/views/instruccion1.html"
-    }, 500)
+    let correo = correoInput.value
+    correo = correo.toLowerCase()
+    let pass = passInput.value
 
 
+    let ref = firebase.database().ref("usuarios/");
+    ref.on('value', function (snapshot) {
+
+        //snapshotVal() recoge todos los elementos del firebase
+        let completeData = snapshot.val();
+
+        //Object.Keys() transforma todos los elemntos en un arreglo
+        let arregloDeDatos = Object.keys(completeData)
+
+        for (let i = 0; i < arregloDeDatos.length; i++) {
+
+            let id = arregloDeDatos[i]
+            let correoDB = completeData[id].correo;
+            let passDB = completeData[id].pass;
 
 
+            if (correo == correoDB) {
+                if (pass == passDB) {
+                    ping.play();
+                    let retrasador = setTimeout(function () {
+                        document.location.href = "./bin/views/instruccion1.html"
+                    }, 500)
+
+                } else {
+                    alert('Contrasena incorrecta')
+
+                }
+
+            }
+
+        }
+
+    }, function (err) {
+        console.log(err)
+    });
 
 })
 
--- bin/sripts/vista/PantallaRegistro.js
+++ bin/sripts/vista/PantallaRegistro.js
@@ -5,8 +5,27 @@ let palabra = document.querySelector("#textoCheck")
 let contenedorModal = document.querySelector(".modalContenedor")
 let modal = document.querySelector(".modal")
 let botonSalir = document.querySelector("#salirBoton")
+let inputNombre = document.querySelector(".nombre")
+let inputCorreo = document.querySelector(".correo")
+let inputContrasena = document.querySelector(".contrasena")
+let inputVerificar = document.querySelector(".verificar")
+let chekmark = document.querySelector(".checkmark")
+let aceptoTerminos = false
 
 let cont = 5;
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBohk7vWZEwnYrclr8UdsfZPDwArRigMqo",
+    authDomain: "familiapi.firebaseapp.com",
+    databaseURL: "https://familiapi.firebaseio.com",
+    projectId: "familiapi",
+    storageBucket: "familiapi.appspot.com",
+    messagingSenderId: "943521700619",
+    appId: "1:943521700619:web:a117acd032fe01862de00e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 /*
 let contadorHilo = setInterval(function(){
@@ -17,6 +36,13 @@ let contadorHilo = setInterval(function(){
     }
     cont-=1;
 },1000);*/
+
+
+chekmark.addEventListener("click",function(){
+ aceptoTerminos=!aceptoTerminos
+})
+
+
 palabra.addEventListener("click", function () {
     modal.classList.add("open")
     contenedorModal.classList.add("open")
@@ -24,10 +50,12 @@ palabra.addEventListener("click", function () {
 })
 
 document.addEventListener("click", function (event) {
-   if(event.target==botonSalir){
-    modal.classList.remove("open")
-    contenedorModal.classList.remove("open")
-   }
+
+    console.log(event.target)
+    if (event.target == botonSalir) {
+        modal.classList.remove("open")
+        contenedorModal.classList.remove("open")
+    }
 
 
 })
@@ -36,7 +64,42 @@ document.addEventListener("click", function (event) {
 
 btnRegistrar.addEventListener("click", function () {
 
-    document.location.href = "./../../bin/views/instruccion1.html"
+    let nombre = inputNombre.value
+    let contrasena = inputContrasena.value
+    let correo = inputCorreo.value
+    correo = correo.toLowerCase()
+    let verificar = inputVerificar.value
+
+    if (contrasena == verificar && correo.includes('@') &&
+        correo.includes('.com') && nombre.length > 0 && contrasena.length > 0 && aceptoTerminos) {
+        firebase.database().ref('usuarios/').push({
+            nombre: nombre,
+            pass: contrasena,
+            correo: correo
+
+        });
+
+        let retraso = setTimeout(function () {
+            document.location.href = "./../../bin/views/instruccion1.html"
+
+        }, 1000)
+
+    } else if (nombre.length == 0) {
+        alert('Ingrese un nombre')
+    } else if (contrasena.length == 0) {
+        alert('Ingrese una contrasena')
+    } else if (correo.includes('@') == false || correo.includes('.com') == false) {
+        alert('Ingrese un correo valido')
+    } else if (contrasena != verificar) {
+        alert('Las contrasenas no coinciden')
+    } else if (aceptoTerminos==false) {
+        alert('Acepta las condiciones')
+    }
+
+
+
+
+
 
 
 
