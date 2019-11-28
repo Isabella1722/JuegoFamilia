170c4836ecda69a1707e8c88e6cfd6c016042472
stban2703 <s.t.ban1401@gmail.com>
2019-11-28 16:16:13 -0500

    aaaa

--- bin/sripts/vista/AdivinanzaResuelta.js
+++ bin/sripts/vista/AdivinanzaResuelta.js
@@ -5,13 +5,14 @@ let btnContinuar = document.querySelector("#botonContinuar")
 let cuerpo = document.querySelector(".Cuerpo")
 let nombre = document.querySelector(".NombreFigura")
 let descripcion = document.querySelector(".NombreCultura")
+let mouseOver = document.querySelector(":hover")
 
 var adivinanza = localStorage.getItem('adivinanza')
 
 
 var logged = localStorage.getItem('logged')
 
-if(logged=='false'){
+if (logged == 'false') {
     document.location.href = "./../../index.html"
 }
 
@@ -26,34 +27,94 @@ switch (adivinanza) {
 
     case 'gueopardo':
         cuerpo.style.backgroundImage = "url(./../../data/img/fondoadivinanza2.png)"
-        nombre.style.color = "black"
-        descripcion.style.color = "black"
-        imagen.src = "./../../data//img/jaguar.png"
-        titulo.textContent = "Guepardo"
+        nombre.style.color = "#BA563C"
+        descripcion.style.color = "#BA563C"
+        imagen.src = "./../../data/img/jaguar.png"
+        titulo.textContent = "Jaguar"
         btnContinuar.style.backgroundColor = "#BA563C"
         cultura.textContent = "Chapa de oro martillada en un molde, en relieve y ensamblada con clavos."
+        //btnContinuar.classList.mouseOver.style.background = "#E09D8B"
         break;
 
     case 'rana':
         cuerpo.style.backgroundImage = "url(./../../data/img/fondoadivinanza3.png)"
-        nombre.style.color = "black"
-        descripcion.style.color = "black"
-        imagen.src = "./../../data//img/rana.png"
+        nombre.style.color = "#AC3317"
+        descripcion.style.color = "#AC3317"
+        imagen.src = "./../../data/img/rana.png"
         titulo.textContent = "Rana"
         btnContinuar.style.backgroundColor = "#AC3317"
+        //mouseOver.style.background = "#DD907E"
         cultura.textContent = "Esta es una alcarraza en forma de anfibio de la región Calima perteneciente al período de Yotoco entre los años 200 a. C. y 1300 d. C."
         break;
 
     case 'armadillo':
         cuerpo.style.backgroundImage = "url(./../../data/img/fondoadivinanza4.png)"
-        nombre.style.color = "black"
-        descripcion.style.color = "black"
+        nombre.style.color = "#594D94"
+        descripcion.style.color = "#594D94"
         imagen.src = "./../../data//img/armadillo.png"
         titulo.textContent = "Armadillo"
         btnContinuar.style.backgroundColor = "#594D94"
+        //mouseOver.style.background = "#A297D7"
         cultura.textContent = "Figura zoomorfa en alcarraza. Cultura Yotoco."
         break;
 }
+
+btnContinuar.addEventListener("mouseover", function () {
+    switch (adivinanza) {
+        case 'aguila':
+            btnContinuar.style.backgroundColor = "rgb(255, 231, 184)"
+            btnContinuar.style.transition = "background 0.3s ease-in-out, box-shadow 0.3s ease-in-out"
+            btnContinuar.style.boxShadow = "0px 0px 0px 7px rgb(255, 231, 184)"
+            break;
+
+        case 'gueopardo':
+            btnContinuar.style.backgroundColor = "#E09D8B"
+            btnContinuar.style.transition = "background 0.3s ease-in-out, box-shadow 0.3s ease-in-out"
+            btnContinuar.style.boxShadow = "0px 0px 0px 7px #E09D8B"
+            break;
+
+        case 'rana':
+            btnContinuar.style.backgroundColor = "#DD907E"
+            btnContinuar.style.transition = "background 0.3s ease-in-out, box-shadow 0.3s ease-in-out"
+            btnContinuar.style.boxShadow = "0px 0px 0px 7px #DD907E"
+            break;
+
+        case 'armadillo':
+            btnContinuar.style.backgroundColor = "#A297D7"
+            btnContinuar.style.transition = "background 0.3s ease-in-out, box-shadow 0.3s ease-in-out"
+            btnContinuar.style.boxShadow = "0px 0px 0px 7px #A297D7"
+            break;
+    }
+
+})
+
+btnContinuar.addEventListener("mouseout", function () {
+    switch (adivinanza) {
+        case 'aguila':
+            btnContinuar.style.backgroundColor = "#F8C65F"
+            btnContinuar.style.transition = "background 0.3s ease-in-out, box-shadow 0.3s ease-in-out"
+            btnContinuar.style.boxShadow = "none"
+            break;
+
+        case 'gueopardo':
+            btnContinuar.style.backgroundColor = "#BA563C"
+            btnContinuar.style.transition = "background 0.3s ease-in-out, box-shadow 0.3s ease-in-out"
+            btnContinuar.style.boxShadow = "none"
+            break;
+
+        case 'rana':
+            btnContinuar.style.backgroundColor = "#AC3317"
+            btnContinuar.style.transition = "background 0.3s ease-in-out, box-shadow 0.3s ease-in-out"
+            btnContinuar.style.boxShadow = "none"
+            break;
+
+        case 'armadillo':
+            btnContinuar.style.backgroundColor = "#594D94"
+            btnContinuar.style.transition = "background 0.3s ease-in-out, box-shadow 0.3s ease-in-out"
+            btnContinuar.style.boxShadow = "none"
+            break;
+    }
+})
 
 btnContinuar.addEventListener("click", function () {
 
